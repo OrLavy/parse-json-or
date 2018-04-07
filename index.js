a3123643c41b3740b262e9e0c81fbf459f09985a
Or Lavy <orlavyil@gmail.com>
2018-04-07 21:28:45 +0300

    Finished basic version

---
+++ index.js
@@ -0,0 +1,19 @@
+const parseJsonOr = require('./lib/parseJsonOr');
+
+/**
+ * Returns the parsed json string or the given object.
+ * @param {string} jsonString The given json string to parse.
+ * @param {any} optionalValue The value that will be returned in case the parsing will fail.
+ * @returns {any} The parsed json string or the given object.
+ */
+exports.orError = parseJsonOr.parseJsonOrError;
+
+/**
+ *
+ * @param {string} parsingError
+ * @param {string} jsonString
+ * @param {function|string|null} [errorBuilder=null] Either a function with the signature of [(parsingError, originalJSONString): string]
+ * or a string that will be used as the error value.
+ * @returns {string}
+ */
+exports.orValue = parseJsonOr.parseJsonOrValue;
